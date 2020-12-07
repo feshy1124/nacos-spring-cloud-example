16c2b18b463854757ea12c491ce34794ddcfb909
fushihai <fushihai@l2cplat.com>
2020-12-07 14:41:55 +0800

    Initial commit

---
+++ nacos-spring-cloud-provider-example/src/main/java/com/platform/nacos/NacosSpringCloudProviderExampleApplication.java
@@ -0,0 +1,13 @@
+package com.platform.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosSpringCloudProviderExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosSpringCloudProviderExampleApplication.class, args);
+    }
+
+}
